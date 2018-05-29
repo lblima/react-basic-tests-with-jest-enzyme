27d44af8fe5e720f666cd91794065f96af0de4a0
Leonardo Lima <leonardo.bruno@outlook.com>
2018-05-29 15:49:26 -0300

    initial setup and add Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { shallow } from 'enzyme';
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  const welcome = <h1 className="App-title">Welcome to React</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
